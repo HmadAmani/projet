e95b73de245e5f0fe6b3ea45937209195c368f45
Amani <ameni.hmad@gmail.com>
2016-03-22 16:18:39 +0100

    init

--- packages/webclient/client/formulaire/form.js
+++ packages/webclient/client/formulaire/form.js
@@ -30,7 +30,88 @@ Template.form.events({
     },
     "change #categorie" :function() {
         categorie = $("#categorie option:selected").text();
-
         Session.set("template",categorie);
+
+        switch(template) {
+            case'Appartement':
+                annonce.type = $('#type').val();
+                annonce.nombrepiece = $('#nombrepiece').val();
+                annonce.surface = $('#surface').val();
+                annonce.placeparking = $('#placeparking').val();
+                annonce.nombresalledebain = $('#nombresalledebain').val();
+
+                break;
+            case 'Maison':
+                annonce.type = $('#type').val();
+                annonce.nombrepiece = $('#nombrepiece').val();
+                annonce.surfaceh = $('#surfaceh').val();
+                annonce.surfacep = $('#surfacep').val();
+                annonce.placeparking = $('#placeparking').val();
+                annonce.nombresalledebain = $('#nombresalledebain').val();
+                break;
+            case'Bureaux et Plateaux':
+                annonce.type = $('#type').val();
+                annonce.nombrepiece = $('#nombrepiece').val();
+                annonce.surface = $('#surface').val();
+                break;
+            case'Magazins et Commerces':
+                annonce.type = $('#type').val();
+                annonce.surface = $('#surface').val();
+                break;
+            case'Terrain':
+                annonce.type = $('#type').val();
+                annonce.surface = $('#surface').val();
+                break;
+            case 'collocation':
+                annonce.surfaceh = $('#surfaceh').val();
+                annonce.meuble = $('#meuble').val();
+                break;
+            case'Voiture':
+                annonce.marque = $('#marque').val();
+                annonce.carrosserie = $('#carrosserie').val();
+                annonce.anneemodel = $('#anneemodel').val();
+                annonce.kilometrage = $('#kilometrage').val();
+                annonce.energie = $('#energie').val();
+                annonce.boitevitesse = $('#boitevitesse').val();
+                annonce.transmission = $('#transmission').val();
+                annonce.puissance = $('#puissance').val();
+                annonce.condition = $('#condition').val();
+                break;
+            case'Fourgonettes':
+                annonce.type = $('#type').val();
+                annonce.anneemodel = $('#marque').val();
+                annonce.kilometrage = $('#kilometrage').val();
+                annonce.energie = $('#energie').val();
+                annonce.puissance = $('#puissance').val();
+                annonce.condition = $('#condition').val();
+             break;
+
+            case'Moto':
+                annonce.carrosserie = $('#carrosserie').val();
+                annonce.anneemodel= $('#anneemodel').val();
+                annonce.kilometrage = $('#kilometrage').val();
+                annonce.cylindre = $('#cylindre').val();
+                annonce.condition = $('#condition').val();
+                break;
+            case'Camions et véhicules indistrulles':
+                annonce.anneemodel = $('#anneemodel').val();
+                annonce.kilometrage = $('#kilometrage').val();
+                annonce.energie = $('#energie').val();
+                annonce.condition = $('#condition').val();
+                break;
+            case'Tracteurs et véhicules agricoles':
+                annonce.anneemodel = $('#anneemodel').val();
+                annonce.kilometrage = $('#kilometrage').val();
+                annonce.energie = $('#energie').val();
+                annonce.condition = $('#condition').val();
+                break;
+            case'Pièces de rechange et accessoires':
+                annonce.type = $('#type').val();
+                annonce.marque = $('#marque').val();
+                annonce.modele = $('#modele').val();
+                annonce.anneemodel = $('#anneemodel').val();
+                annonce.condition = $('#condition').val();
+
+        }
     }
 });
