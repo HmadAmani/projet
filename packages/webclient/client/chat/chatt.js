849d99f1fac09e60f8b5caac7ab94dce2f474c04
Amani <ameni.hmad@gmail.com>
2016-04-04 00:14:41 +0100

    init

---
+++ packages/webclient/client/chat/chatt.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Amani on 04/04/2016.
+ */
+(function() {
+
+    $('#live-chat header').on('click', function() {
+
+        $('.chat').slideToggle(300, 'swing');
+        $('.chat-message-counter').fadeToggle(300, 'swing');
+
+    });
+
+    $('.chat-close').on('click', function(e) {
+
+        e.preventDefault();
+        $('#live-chat').fadeOut(300);
+
+    });
+
+}) ();
