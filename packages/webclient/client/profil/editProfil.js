8282ed9bee7c2e7dcc362ee60b10c5de8c55dc11
Amani <ameni.hmad@gmail.com>
2016-05-04 01:44:48 +0100

    init

---
+++ packages/webclient/client/profil/editProfil.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Amani on 04/05/2016.
+ */
+
+Template._loginButtonsLoggedInDropdown.events({
+
+    'click #login-buttons-profile': function(event) {
+        Router.go('profile');
+    }
+});
